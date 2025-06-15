55074a3a763595009d3cb7e89796883bc805a970
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-15 11:09:44 +0530

    new file:   src/components/RohitAvatar.jsx

---
+++ src/components/RohitAvatar.jsx
@@ -0,0 +1,12 @@
+// src/components/RohitAvatar.jsx
+import React from "react";
+
+const RohitAvatar = () => {
+  return (
+    <div style={{ width: 50, height: 50, backgroundColor: "gray", borderRadius: "50%" }}>
+      {/* Placeholder avatar */}
+    </div>
+  );
+};
+
+export default RohitAvatar;
